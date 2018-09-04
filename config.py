9be9faaf2d986bb25f141f5282c9cf20f02fbd11
fabischapeli <fabischapeli97@gmail.com>
2018-09-04 16:33:31 +0300

    [ch] set up config.py file

--- config.py
+++ config.py
@@ -9,12 +9,10 @@ class Config(object):
     TESTING = False
     SECRET_KEY = "jfsahe8r934"
 
-
 class Testing(Config):
     """Contains additional config variables required during testing"""
     DEBUG = True
     TESTING = True
-
 
 class Development(Config):
     """Contains additional config variables required during development"""
