9f38f360f6f05391217e6718af0a5bb60f1cb29b
fabischapeli <fabischapeli97@gmail.com>
2018-09-17 09:42:47 +0300

    chore separating tests in different modules [Finishes #160543306]

--- tests/base_setup.py
+++ tests/base_setup.py
@@ -8,6 +8,7 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from app import create_app
 
 
+
 class BaseTest(unittest.TestCase):
     """
     This is a base class to be inherited by other test classes
