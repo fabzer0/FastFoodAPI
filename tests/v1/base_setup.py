1c0a1ea280331d92f0f9795742a451de131baddc
fabischapeli <fabischapeli97@gmail.com>
2018-09-26 07:02:58 +0300

    [chore #16823202] removed unnecessary lines from my files

--- tests/v1/base_setup.py
+++ tests/v1/base_setup.py
@@ -1,8 +1,11 @@
 """
 This module sets up information needed for testing
 """
+import os
+import sys
 import unittest
 from app.base import create_app
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 class BaseTest(unittest.TestCase):
     """
