6a5b0303d9e0792425786a3a614cd08e4ff11e4b
fabischapeli <fabischapeli97@gmail.com>
2018-10-04 19:46:23 +0300

    [chore] fix fail heroku build

--- app/v2/models/createdb.py
+++ app/v2/models/createdb.py
@@ -6,7 +6,7 @@ def connect_to_db(config=None):
     Function to connect to the required db
     """
 
-    return psycopg2.connect(os.getenv('DB_URL'))
+    return psycopg2.connect(os.getenv('DATABASE_URL'))
 
 
 
@@ -60,4 +60,3 @@ def main(config=None):
     cur.close()
     conn.close()
     print('database successfully created')
-
