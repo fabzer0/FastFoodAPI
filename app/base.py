0156cead82f55c12f25b9c2cc0da2075c6be0463
fabischapeli <fabischapeli97@gmail.com>
2018-10-08 21:32:02 +0300

    [chore] include CORS in file

--- app/base.py
+++ app/base.py
@@ -1,12 +1,15 @@
 from flask import Flask, redirect
+from flask_cors import CORS
 from app.v2.resources.menu import menu_api
 from app.v2.resources.orders import orders_api
 from app.v2.resources.users import users_api
 from app.v2.resources.meals import meals_api
 
+
 def create_app():
     from app.v2.models.createdb import main
     app = Flask(__name__)
+    CORS(app)
     app.config.from_object('instance.v2.config.DevelopmentEnv')
     app.url_map.strict_slashes = False
 
