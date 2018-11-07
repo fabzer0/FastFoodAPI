41264e21e564826ac2b5fc94fa997e2c3b74c5b1
fabischolasi <fabischapeli97@gmail.com>
2018-11-07 20:42:52 +0300

    [bug #161784625] make ui changes in user menu view page

---
+++ build/templates/scripts/app.js
@@ -0,0 +1,6 @@
+// function showCart() => {
+//     let div = document.getElementById('hide-show-cart');
+//     if (if div.style.display === 'none') {
+//         div.style.display = 'block';
+//     }
+// }
