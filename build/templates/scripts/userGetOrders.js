6dea76cbf6e27128c2aa66554488594340e75da7
fabischolasi <fabischapeli97@gmail.com>
2018-11-08 23:49:22 +0300

    [chore] consuming getting order route

--- build/templates/scripts/userGetOrders.js
+++ build/templates/scripts/userGetOrders.js
@@ -1,44 +1,51 @@
 const orders = document.getElementById('orders');
-var request = new XMLHttpRequest();
+let br = document.createElement('br');
+const token = localStorage.getItem('token');
 
-// Getting the menu
-request.open('GET', 'https://fast-food-fast-v2-api.herokuapp.com/api/v2/user/orders', true);
-request.onload = function () {
-    var data = JSON.parse(this.response);
-    if (request.status >= 200 && request.status < 400) {
-        data.forEach(order => {
-            let cartHolder = document.getElementById('cartholder');
-            let br = document.createElement('br');
+// Getting the orders
+fetch('https://fast-food-fast-v2-api.herokuapp.com/api/v2/user/orders', {
+  method: 'get',
+  headers: { 'Content-Type': 'application/json; charset=utf-8', 'x-access-token': token }
+})
+  .then(response => response.json())
+  .then(response => {
+    if (true) {
+      response.forEach(orderItem => {
+        let cartHolder = document.getElementById('cartholder');
+        
 
-            let spanOne = document.createElement('span');
-            spanOne.setAttribute('id', 'one');
-            spanOne.textContent = order.item;
+        let spanOne = document.createElement('span');
+        spanOne.setAttribute('id', 'one');
+        spanOne.textContent = orderItem.item;
 
-            let spanTwo = document.createElement('span');
-            spanTwo.setAttribute('id', 'two');
-            spanTwo.textContent = order.price;
+        let spanTwo = document.createElement('span');
+        spanTwo.setAttribute('id', 'two');
+        spanTwo.textContent = orderItem.price;
 
-            let spanThree = document.createElement('span');
-            spanThree.setAttribute('id', 'three');
-            spanThree.textContent = order.status;
+        let spanThree = document.createElement('span');
+        spanThree.setAttribute('id', 'three');
+        spanThree.textContent = orderItem.status;
 
-            let spanFour = document.createElement('span');
-            spanFour.setAttribute('id', 'four');
-            spanFour.textContent = order.price;
+        let spanFour = document.createElement('span');
+        spanFour.setAttribute('id', 'four');
+        spanFour.textContent = orderItem.price;
 
-            cartHolder.appendChild(br);
-            cartHolder.appendChild(spanOne);
-            cartHolder.appendChild(spanTwo);
-            cartHolder.appendChild(spanThree);
-            cartHolder.appendChild(spanThree);
+        cartHolder.appendChild(br);
+        cartHolder.appendChild(spanOne);
+        cartHolder.appendChild(spanTwo);
+        cartHolder.appendChild(spanThree);
+        cartHolder.appendChild(spanFour);
 
-            orders.appendChild(cartHolder);
-        });
+        orders.appendChild(cartHolder);
+        console.log(response.message);
+        console.log(data.message);
+      });
     } else {
+      console.log(response.message)
       const errorMessage = document.createElement('div');
       errorMessage.setAttribute('id', 'menu-error');
-      errorMessage.textContent = data.message;
-      menuColumn.appendChild(errorMessage);
+      errorMessage.textContent = response.message;
+      mealColumn.appendChild(errorMessage);
     }
-}
-request.send();
+  })
+
