d37e993e57deb2e268fe739758e32752f8865295
fabischolasi <fabischapeli97@gmail.com>
2018-11-09 20:48:56 +0300

    [Finishes #161160076] user can be able to view the orders that are in the cart

--- build/templates/scripts/userGetOrders.js
+++ build/templates/scripts/userGetOrders.js
@@ -1,5 +1,4 @@
 const orders = document.getElementById('orders');
-let br = document.createElement('br');
 const token = localStorage.getItem('token');
 
 // Getting the orders
@@ -11,41 +10,30 @@ fetch('https://fast-food-fast-v2-api.herokuapp.com/api/v2/user/orders', {
   .then(response => {
     if (true) {
       response.forEach(orderItem => {
-        let cartHolder = document.getElementById('cartholder');
+        let cartHolder = document.createElement('div');
+        cartHolder.setAttribute('id', 'cartholder');
         
-
+        let br = document.createElement('br');
         let spanOne = document.createElement('span');
         spanOne.setAttribute('id', 'one');
         spanOne.textContent = orderItem.item;
 
         let spanTwo = document.createElement('span');
         spanTwo.setAttribute('id', 'two');
-        spanTwo.textContent = orderItem.price;
+        spanTwo.textContent = 'Status: ' + orderItem.status;
 
         let spanThree = document.createElement('span');
         spanThree.setAttribute('id', 'three');
-        spanThree.textContent = orderItem.status;
-
-        let spanFour = document.createElement('span');
-        spanFour.setAttribute('id', 'four');
-        spanFour.textContent = orderItem.price;
+        spanThree.textContent = 'Total: ' + 'Ksh. ' + orderItem.totalprice;
 
         cartHolder.appendChild(br);
         cartHolder.appendChild(spanOne);
         cartHolder.appendChild(spanTwo);
         cartHolder.appendChild(spanThree);
-        cartHolder.appendChild(spanFour);
 
         orders.appendChild(cartHolder);
-        console.log(response.message);
-        console.log(data.message);
+      
       });
-    } else {
-      console.log(response.message)
-      const errorMessage = document.createElement('div');
-      errorMessage.setAttribute('id', 'menu-error');
-      errorMessage.textContent = response.message;
-      mealColumn.appendChild(errorMessage);
-    }
+    } 
   })
 
